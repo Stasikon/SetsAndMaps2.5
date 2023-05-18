e44a3a1eb17bc1f98bb436946047e251cc046dd9
Heppy <lecsa515@gmail.com>
2023-05-18 13:59:58 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     private static final List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
     public static void main(String[] args) {
         task1();
-      //  task2();
+        task2();
        // task3();
        // task4();
     }
@@ -19,7 +19,19 @@ public class Main {
             }
             System.out.println();
         }
-
+    public static void task2() {
+        System.out.println("Задание 2");
+        Set<Integer> numSet = new TreeSet<>();
+        for (Integer integer : nums) {
+            if (integer % 2 == 0) {
+                numSet.add(integer);
+            }
+        }
+        for (Integer numsSet : numSet) {
+            System.out.println(numsSet);
+        }
+        System.out.println();
+    }
 
 
     }
